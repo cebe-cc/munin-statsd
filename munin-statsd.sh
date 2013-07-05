1940d8c706b3debab037acf6379badcf0d8ae04d
Carsten Brandt <mail@cebe.cc>
2013-07-05 04:09:33 +0200

    remove useless crashlog

--- munin-statsd.sh
+++ munin-statsd.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -e
 
-LOGFILE=/var/log/munin-statsd.crashlog
-
 ulimit -Sv 262144 # 256 MB
 
 ulimit -St 10 # 10 seconds CPU time
 
-perl /srv/munin-statsd/munin-statsd.pl || env >> $LOGFILE
+perl /srv/munin-statsd/munin-statsd.pl
