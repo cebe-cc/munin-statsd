e8549fa6ee442dee49f10226b64aa44ef564715f
Carsten Brandt <mail@cebe.cc>
2013-07-05 04:01:20 +0200

    Softlimit instead of Hardlimit
    hardlimit fails for unknown reason

--- munin-statsd.sh
+++ munin-statsd.sh
@@ -2,8 +2,8 @@
 
 LOGFILE=/var/log/munin-statsd.crashlog
 
-ulimit -Hv 65536 # 64 MB
+ulimit -Sv 65536 # 64 MB
 
-ulimit -Ht 10 # 10 seconds CPU time
+ulimit -St 10 # 10 seconds CPU time
 
 perl /srv/munin-statsd/munin-statsd.pl || env >> $LOGFILE
